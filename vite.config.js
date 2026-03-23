e0517e4804e208787aafef8264413b8bbbc6b334
Metaniya Shiferaw <125639579+Metaniya@users.noreply.github.com>
2026-03-23 15:36:46 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/React-Project/", // <--- THIS MUST MATCH YOUR REPO NAME EXACTLY
+  base: "/React-Course/", 
 })
